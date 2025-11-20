d87bfe97688debc0468911f0ce94908580dfa26b
SMHASSANALI <smhassanali@gmail.com>
2025-11-20 05:44:25 +0500

    Once more updated the logo path

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <div className="w-full bg-white h-34 flex items-center justify-center overflow-hidden py-6 px-4">
       <div className="max-w-7xl mx-auto flex flex-row items-center justify-between w-full">
         <div className="relative">
-          <Image src="/logo.jpg" alt="logo" className="object-fill" width="180" height="100"/>
+          <Image src="/Logo.jpg" alt="logo" className="object-fill" width="180" height="100"/>
         </div>
         <div className="flex flex-row items-center justify-center gap-8 text-sm">
           <div className="uppercase">Home</div>
